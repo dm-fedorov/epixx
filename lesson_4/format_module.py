6936334e782b141dbee4b59d49bb78d33c4cf83e
Dmitriy Fedorov <dmitriy.fedoroff@gmail.com>
2018-09-21 10:45:34 +0300

    4 занятие

--- lesson_4/format_module.py
+++ lesson_4/format_module.py
@@ -3,9 +3,5 @@ def f(var1, var2):
     '''
     Возвращает строку вида "var1 и var2"
     '''
-    # проверяем версию Python для работы с f-строками
-    import sys
-    if sys.version_info[:2] >= (3, 6):
-        return f'{var1} и {var2}'
-    else:
-        return '{0} и {1}'.format(var1, var2)
+    return '{0} и {1}'.format(var1, var2)
+   #return f'{var1} и {var2}' # для версии 3.6 и выше
