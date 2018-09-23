0d904df3018ad58e19a349cf6189fcb7619dbc39
Dmitriy Fedorov <dmitriy.fedoroff@gmail.com>
2018-09-23 10:15:26 +0300

    2

---
+++ lesson_2/table.py
@@ -0,0 +1,16 @@
+# table.py
+value = input("Введите атомный номер элемента: ")
+if len(value) > 0:
+    num = int(value)
+    if num == 3:        
+        print("Li, Литий")
+    elif num == 17:
+        print("Cl, Хлор")
+    elif num == 25:
+        print("Mn, Марганец")
+    elif num == 80:
+        print("Hg, Ртуть")        
+    else:
+        print("Не могу определить, что это.")
+else:
+    print("Введите значение.")
