e98508ca674dd310d1cb9fde87213e5c69f1c616
Dmitriy Fedorov <dmitriy.fedoroff@gmail.com>
2018-10-02 18:37:30 +0300

    game

---
+++ lesson_8/delimiter_test.py
@@ -0,0 +1,23 @@
+# delimiter_test.py
+def check_delimiter(s):
+    '''
+    >>> check_delimiter("( ) { [ ] ( ) { } }")    
+    True
+
+    >>> check_delimiter("( ) { [ ] }")    
+    True
+
+    >>> check_delimiter("( ) { [ ] ( ] { } }")    
+    False
+
+    >>> check_delimiter("( ) ) [ ] ( ] { } }")    
+    False
+    
+    '''
+
+    # ...
+    
+    return # ... 
+
+import doctest
+doctest.testmod()
