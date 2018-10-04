937a2c2e593516b52404eda8229e58ef513c7204
Dmitriy Fedorov <dmitriy.fedoroff@gmail.com>
2018-10-04 06:49:17 +0300

    fix

--- lesson_6/list_range.py
+++ lesson_6/list_range.py
@@ -1,8 +1,8 @@
 # list_range.py
 a = []
-for i in range(1,15):
+for i in range(1, 15):
     a.append(i)
 print(a)
 
 print(list(range(1, 15)))
-print([i for i in range(1,15)])
+print([i for i in range(1, 15)])
