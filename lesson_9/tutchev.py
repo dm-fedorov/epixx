e215a7439734673823d5246f4d31052f2a9842d0
dmf <info@dfedorov.spb.ru>
2018-10-06 12:22:26 +0300

    html

--- lesson_9/tutchev.py
+++ lesson_9/tutchev.py
@@ -6,28 +6,7 @@ def get_html(text, img):
     Функция для генерации html-документа.
     '''
     try:
-        with open('file.html', 'w', encoding='utf-8') as f:
-            f.write('''
-            <!DOCTYPE html>
-    <html>
-        <head>
-           <meta charset="utf-8">
-           <title>Стихи</title>
-        </head>
-        <body>''')
-
-            url = text
-            with urllib.request.urlopen(url) as webpage:
-                for line in webpage:
-                    line = line.strip()
-                    line = line.decode('utf-8')
-                    f.write(line)
-                    f.write("<br>")
-              
-            f.write('''
-      <p><img src=''' + img + ''' alt="Тютчев"></p>
-        </body>
-    </html>''')
+        # здесь код
 
     except Exception as error:
         print(error)        
