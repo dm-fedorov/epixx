8edffd7f409b9d838fe121ec8018c9d8d452f3c7
Dmitriy Fedorov <dmitriy.fedoroff@gmail.com>
2018-11-20 11:56:03 +0300

    fix

--- lesson_4/format_str.py
+++ lesson_4/format_str.py
@@ -5,6 +5,7 @@ def f1(var1, var2):
 def f2(var1, var2):
     return '{0} и {1}'.format(var1, var2)
 
+# с версии 3.6:
 def f3(var1, var2):
     return f'{var1} и {var2}'
 
--- lesson_4/num_pi.py
+++
@@ -1,9 +0,0 @@
-# num_pi.py
-def fun(n):
-    import math
-    return f'{math.pi:.{n}f}' # >= 3.6
-
-if __name__ == '__main__':
-    print(fun(6))
-
-
