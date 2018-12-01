80ffa022bf3c25d389e3b07d02e27e7b4c7ff2db
dmf <info@dfedorov.spb.ru>
2018-12-01 23:09:37 +0300

    fix

--- lesson_7/keywords.py
+++ lesson_7/keywords.py
@@ -1,15 +1,25 @@
 # keywords.py
-def total(initial=5, *numbers, **keywords):    
-    count = initial 
+def total(init=1, *numbers, **keywords):    
+    """
+    Когда мы объявляем параметр со звёздочкой (например, *param),
+    все позиционные аргументы начиная с этой позиции и до конца
+    будут собраны в кортеж под именем param. Аналогично, когда мы
+    объявляем параметры с двумя звёздочками (**param), все ключевые
+    аргументы начиная с этой позиции и до конца будут собраны в словарь
+    под именем param.
+    """
+    count = init # начальное значение счетчика суммы чисел 
     for number in numbers: 
-        count += number 
+        count = count + number # count += number
     for key in keywords: 
-        count += keywords[key] 
+        count = count + keywords[key] 
     return count 
 
 if __name__ == '__main__':
-    print(total()) # initial=5
-    print(total(2, 1)) # initial=2
-    print(total(2, 1, key=3)) # initial=2
-    print(total(10, 1, 2, 3, vegetables=50, fruits=100)) 
+    print(total()) # init=1
+    print(total(init=7)) # init=7
+    print(total(2)) # init=2
+    print(total(2, 1))
+    print(total(2, 1, num=5))
+    print(total(10, 1, 2, num1=4, num2=6)) 
 
