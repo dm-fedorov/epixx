3f41ca8dff30e52ad6f9b947161106bf6a049bd4
dmf <info@dfedorov.spb.ru>
2018-12-12 20:53:19 +0300

    fix

--- lesson_10/mytk02.py
+++ lesson_10/mytk02.py
@@ -2,8 +2,10 @@
 import tkinter
 window = tkinter.Tk()
 label = tkinter.Label(window, text = "Это текст в окне!")
-
 label.pack()
+
+label1 = tkinter.Label(window, text = "Привет, мир!")
+label1.pack()
 
 window.mainloop()
 
